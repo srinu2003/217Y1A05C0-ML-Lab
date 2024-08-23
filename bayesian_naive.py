c5744cad3ffe6ce9337c26339cee0d785ece23f6
srinu2003 <tsrin2003@gmail.com>
2024-08-23 23:45:57 +0530

    feat: Add code for Bayesian Naive algorithm and database connection

---
+++ bayesian_naive.py
@@ -0,0 +1,4 @@
+print("217Y1A05C0")
+paf = float(input("Enter value of PAF: "))
+pf = float(input("Enter value of F: "))
+print(f"The probability that the student is given absent on friday {paf/pf*100}%")
